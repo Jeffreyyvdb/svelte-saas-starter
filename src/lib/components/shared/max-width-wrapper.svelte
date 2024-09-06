188ea66c1eb0c3394fa554a1597519e07b931c86
Jeffreyyvdb <Jeffrey.2001.2312@gmail.com>
2024-09-07 00:39:57 +0200

    max width wrapper

---
+++ src/lib/components/shared/max-width-wrapper.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+
+	interface Props {
+		class?: string;
+		large?: boolean;
+		children?: any;
+	}
+
+	const { class: className = '', large = false, children }: Props = $props();
+</script>
+
+<div class={cn('container', large ? 'max-w-screen-2xl' : 'max-w-6xl', className)}>
+	{@render children()}
+</div>
