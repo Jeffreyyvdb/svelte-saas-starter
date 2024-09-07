c9b57323be397f78a5a93730fae7550cdf369efb
Jeffreyyvdb <Jeffrey.2001.2312@gmail.com>
2024-09-07 22:40:58 +0200

    Dashboard header

---
+++ src/lib/components/shared/dashboard-header.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	interface DashboardHeaderProps {
+		heading: string;
+		text?: string;
+		children?: any;
+	}
+
+	const { heading, text, children }: DashboardHeaderProps = $props();
+</script>
+
+<div class="flex items-center justify-between">
+	<div class="grid gap-1">
+		<h1 class="font-heading text-2xl font-semibold">{heading}</h1>
+		{#if text}
+			<p class="text-base text-muted-foreground">{text}</p>
+		{/if}
+	</div>
+	{@render children()}
+</div>
--- src/routes/(protected)/dashboard/+page.svelte
+++ src/routes/(protected)/dashboard/+page.svelte
@@ -1,4 +1,18 @@
 <script lang="ts">
+	import DashboardHeader from '$lib/components/shared/dashboard-header.svelte';
+	import { Button } from '$lib/components/ui/button';
 </script>
 
-<h1>Dashboard</h1>
+<DashboardHeader
+	heading="Dashboard"
+	text={`Current Role : ADMIN — Change your role in settings.`}
+/>
+
+<!-- <EmptyPlaceholder>
+	<EmptyPlaceholder.Icon name="post" />
+	<EmptyPlaceholder.Title>No content created</EmptyPlaceholder.Title>
+	<EmptyPlaceholder.Description>
+		You don&apos;t have any content yet. Start creating content.
+	</EmptyPlaceholder.Description>
+	<Button>Add Content</Button>
+</EmptyPlaceholder> -->
